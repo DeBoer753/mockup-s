fd63b6d09b0c30e2e00a0962d527badc4a128f11
Myles DeBoer <myles.deboer@gmail.com>
2023-03-30 17:04:30 -0700

    inital commit

---
+++ assets/script.js
@@ -0,0 +1,118 @@
+// INTERACTIVE HAMBURGER ICON:
+const hamburger = document.querySelector('.hamburger')
+const navBar = document.querySelector('.anchorBox')
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navBar.classList.toggle('active');
+})
+
+// SCROLL ON CLICK:
+// navbar ids
+
+let homeLogo = document.querySelector('#homeLogo')
+let homeSleepless = document.querySelector('#homeSleepless')
+let mission = document.querySelector('#mission')
+let services = document.querySelector('#services')
+let team = document.querySelector('#team')
+let photos = document.querySelector('#photos')
+let contact = document.querySelector('#contact')
+
+// section ids
+let scrollToBanner = document.querySelector('#scrollToBanner')
+let scrollToHomeSleepless = document.querySelector('#scrollToHomeSleepless')
+let scrollToMission = document.querySelector('#scrollToMission')
+let scrollToServices = document.querySelector('#scrollToServices')
+let scrollToTeam = document.querySelector('#scrollToTeam')
+let scrollToPhotos = document.querySelector('#scrollToPhotos')
+let scrollToContact = document.querySelector('#scrollToContact')
+
+// scroll to click
+homeLogo.addEventListener('click', () => {
+  scrollToBanner.scrollIntoView({behavior: 'smooth'})
+  console.log('clicked');
+})
+homeSleepless.addEventListener('click', () => {
+  scrollToBanner.scrollIntoView({behavior: 'smooth'})
+  console.log('clicked');
+})
+mission.addEventListener('click', () => {
+  scrollToMission.scrollIntoView({behavior: 'smooth'})
+  console.log('clicked');
+})
+
+services.addEventListener('click', () => {
+  scrollToServices.scrollIntoView({behavior: 'smooth'})
+  console.log('clicked');
+})
+
+team.addEventListener('click', () => {
+  scrollToTeam.scrollIntoView({behavior: 'smooth'})
+})
+
+contact.addEventListener('click', () => {
+  scrollToContact.scrollIntoView({behavior: 'smooth'})
+})
+
+photos.addEventListener('click', () => {
+  scrollToPhotos.scrollIntoView({behavior: 'smooth'})
+})
+
+
+// BANNER SLIDESHOW:
+
+let slideShow = () => {
+
+  let slider = document.querySelector(".slider"); // class
+  let sliderWidth = slider.offsetWidth;
+  let slideList = document.querySelector("#sliderBox");
+  let count = 1;
+  let items = slideList.querySelectorAll("li").length;
+  let prev = document.getElementById("prev");
+  let next = document.getElementById("next");
+  
+  window.addEventListener('', function() {
+    sliderWidth = slider.offsetWidth;
+  });
+  
+  let prevSlide = function() {
+    if(count > 1) {
+      count = count - 2;
+      slideList.style.left = "-" + count * 10 + "px";
+      count++;
+    }
+    else if(count = 1) {
+      count = items - 1;
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    }
+  };
+  
+  let nextSlide = function() {
+    if(count < items) {
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    }
+    else if(count = items) {
+      slideList.style.left = "0px";
+      count = 1;
+    }
+  };
+  
+  next.addEventListener("click", function() {
+    nextSlide();
+  });
+  
+  prev.addEventListener("click", function() {
+    prevSlide();
+  });
+  
+  setInterval(function() {
+    nextSlide()
+  }, 2000);
+  
+  };
+  
+  window.onload = function() {
+  slideShow();  
+  }
